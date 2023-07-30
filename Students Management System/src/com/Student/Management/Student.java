05f865a359c1a666b97c275e242654bda7e83ec1
Salman-S7 <sammyzed1505@gmail.com>
2023-07-30 17:17:18 +0530

    first commit did very simple things just created project and added connector libraries(mysql)

--- Students Management System/src/com/Student/Management/Student.java
+++ Students Management System/src/com/Student/Management/Student.java
@@ -1,11 +1,30 @@
 package com.Student.Management;
 
+import java.sql.*;
+
 public class Student {
+    static String deburl = "jdbc:mysql://localhost:3306/students";
+    static String username = "root";
+    static String password = "shaikh0099";
     public static void main(String[] args) {
-        try {
-            Class.forName("");
+        try (Connection connection = DriverManager.getConnection(deburl,username,password)){
+            //registring driver class
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //creating connection
+            System.out.println("connection established");
+
+            //write whatever logic you want to write here==>
+
+
+
+
+
         } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 }
+
